c43a5d5983e69583cde95a523987e451cee83f65
Debajyotir <109982301+Debajyotir@users.noreply.github.com>
2023-01-11 01:12:58 +0530

    Create 13_Airplane Seat Assignment Probability.cpp

---
+++ Microsoft/13_Airplane Seat Assignment Probability.cpp
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/airplane-seat-assignment-probability/
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        if(n==1)
+            return 1;
+        return .5;
+    }
+};
+//submission link
+//https://leetcode.com/problems/airplane-seat-assignment-probability/submissions/873562720/
